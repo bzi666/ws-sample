c78c63d310c08ee98e926de5c6c9134d64d61ffb
bzi666 <1171553297@qq.com>
2021-11-11 18:51:01 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,80 +1,49 @@
 define([],function() {
-    "workflowApiVersion": "1.1",
-    "metaData": {
-       "icon": "favicon.ico",
-       "category": "message"
+    workflowApiVersion: "1.1",
+    metaData: {
+       icon: "favicon.ico",
+       category: "message"
     },
-    "type": "REST",
-    "lang": {
-       "en-US": {
-           "name": "REST Activity (Workflow API v1.1)",
-           "description": "An example REST activity using workflow API v1.1 format."
+    type: "REST",
+    lang: {
+       en-US: {
+           name: "REST Activity (Workflow API v1.1)",
+           description: "An example REST activity using workflow API v1.1 format."
        }
     },
-    "arguments": {
-        "execute": {
-            "inArguments": [],
-            "url": "https://ens2pfv4xtvpwxi.m.pipedream.net",        
-            "headers": "{\"Authorization\":\"Bearer theToken\"}"
+    arguments: {
+        execute: {
+            inArguments: [],
+            url: "https://ens2pfv4xtvpwxi.m.pipedream.net",        
+            headers: "{\"Authorization\":\"Bearer theToken\"}"
         }
     },
-    "configurationArguments": {
-       "save": {
-           "url": "https://ens2pfv4xtvpwxi.m.pipedream.net/save"
+    configurationArguments: {
+       save: {
+           url: "https://ens2pfv4xtvpwxi.m.pipedream.net/save"
        },
-       "publish": {
-           "url": "https://ens2pfv4xtvpwxi.m.pipedream.net/publish",
+       publish: {
+           url: "https://ens2pfv4xtvpwxi.m.pipedream.net/publish",
       
        },
-       "validate": {
+       validate: {
            "url": "https://ens2pfv4xtvpwxi.m.pipedream.net/validate"
        },
-       "stop": {
-           "url": "https://ens2pfv4xtvpwxi.m.pipedream.net/stop"
+       stop: {
+           url: "https://ens2pfv4xtvpwxi.m.pipedream.net/stop"
        }
     },
-    "wizardSteps": [
-       { "label": "Step 1", "key": "step1" },
-       { "label": "Step 2", "key": "step2" },
-       { "label": "Step 3", "key": "step3" },
-       { "label": "Step 4", "key": "step4", "active": false }
+    wizardSteps: [
+       { label: "Step 1", key: "step1" },
+       { label: "Step 2", key: "step2" },
+       { label: "Step 3", key: "step3" },
+       { label: "Step 4", key: "step4", active: false }
     ],
-    "userInterfaces": {
-       "configModal": {
-           "height": 200,
-           "width": 300,
-           "fullscreen": true
+    userInterfaces: {
+       configModal: {
+           height: 200,
+           width: 300,
+           fullscreen: true
        }
     },
-    "schema": {
-        "arguments": {
-            "execute": {
-                "inArguments": [
-                    {
-                        "phoneNumber": {
-                            "dataType": "Phone",
-                            "isNullable": false,
-                            "direction": "in"   
-                        }
-                    },
-                    {
-                        "emailAddress": {
-                            "dataType": "Email",
-                            "isNullable": false,
-                            "direction": "in"
-                        }
-                    }
-                ],
-                "outArguments": [
-                    {
-                        "foundSignupDate": {
-                            "dataType": "Date",
-                            "direction": "out",
-                            "access": "visible"
-                        }
-                    }
-                ]
-            }
-        }
-    }
 });
